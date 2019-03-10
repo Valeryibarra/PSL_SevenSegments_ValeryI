9eaecf6da595bc97c81bdef5ce79d05702969644
valeryibarra <valeryibarra2@gmail.com>
2019-03-10 12:37:03 -0500

    First commit with Core and CoreTest class. First functional product

---
+++ SevenSegments/src/logical/Core.java
@@ -0,0 +1,196 @@
+package logical;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Core {
+
+    /**
+     * Reader that get the inputs 
+     */
+    private static BufferedReader reader;
+	
+    /**
+     * Writer that shows the outputs
+     */
+    private static BufferedWriter writer;
+    
+    private static String totalResult;
+	
+	/**
+	 * Main method of the class
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// First line of input
+		String line;
+		try {
+			line = reader.readLine();
+			// Current digit that will be found
+			Character currenDigit = ' ';
+			totalResult="";
+			
+			//Flag that indicates if the while sentence should follow or not
+			boolean whileFlag = true;
+
+			while ( line != null && !line.equals(" ")  && whileFlag) {
+				if(!totalResult.equals("")) {
+					totalResult+="\r\n";				
+				}
+
+				String firstLine = line;
+				int sizeFirstLine = firstLine.length();
+				line = reader.readLine();
+
+				String secondLine = line;
+				int sizeSecondLine = secondLine.length();
+				line = reader.readLine();
+
+				String thirdLine = line;
+				int sizethirdLine = thirdLine.length();
+
+				if (sizeFirstLine != sizeSecondLine || sizeSecondLine != sizethirdLine) {
+					// The lines has different size, can not find the numbers
+					whileFlag = false;
+					writer.write("For the input you must enter 3 lines of the same size");
+					totalResult="For the input you must enter 3 lines of the same size";
+					writer.flush();
+
+				} else {
+
+					//Array with the high, middle and low part of the digit(number) respectively
+					String[] digitArray = new String[3];
+
+					//Every 4 characters read a new digit
+					for (int i = 0; i < firstLine.length(); i += 4) {
+											
+						//Is the top part of the digit
+						digitArray[0] = firstLine.substring(i, i + 3);
+						//Is the middle part of the digit
+						digitArray[1] = secondLine.substring(i, i + 3);
+						//Is the bottom part of the digit
+						digitArray[2] = thirdLine.substring(i, i + 3);
+						
+						//The digit is complete but divided into 3 parts.				
+						
+						//Get the character of the digit
+						currenDigit = getDigitNumber(digitArray);
+
+						writer.write(currenDigit + "");
+						totalResult+=currenDigit;
+
+					}
+
+					writer.write("\n");
+					writer.flush();
+					line = reader.readLine();
+					if(line==null || line.equals(" ") ) {
+						whileFlag=false;
+					}
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	/**
+	 * Method that returns the number obtained by joining the array in which the digit is divided into parts
+	 * 
+	 * @param digitArray Array with the high, middle and low part of the digit(number) respectively
+	 * @return Character Digit that represents the array
+	 */
+	public static Character getDigitNumber(String[] digitArray) {
+
+		// Each digit has 3 lines and each line has a string of 3 Characters
+		//Is the top part of the digit
+		String firstLine = digitArray[0];
+		//Is the middle part of the digit
+		String secondLine = digitArray[1];
+		//Is the bottom part of the digit
+		String thirdLine = digitArray[2];
+
+		// Is the digit obtained by joining each part of the array
+		// It is assumed all digits are NaN(Not a Number) until it fits in a number, the
+		// digit of NaN is x
+		Character digit = 'x';
+		
+		switch (firstLine) {
+		case "   ":
+			// Digit is 1, 4 or NaN
+			if (secondLine.equals("  |") && thirdLine.equals("  |")) {
+				digit = '1';
+			} else if (secondLine.equals("|_|") && thirdLine.equals("  |")) {
+				digit = '4';
+			}
+			break;
+		case " _ ":
+			// Digit is 2,3,5,6,7,8,9,0 or NaN
+			switch (secondLine) {
+			case " _|":
+				// Digit is 2, 3 or NaN
+				switch (thirdLine) {
+				case "|_ ":
+					digit = '2';
+					break;
+				case " _|":
+					digit = '3';
+					break;
+				}
+				break;
+			case "|_|":
+				// Digit is 8, 9 or NaN
+				switch (thirdLine) {
+				case "|_|":
+					digit = '8';
+					break;
+				case "  |":
+					digit = '9';
+					break;
+				}
+				break;
+			case "|_ ":
+				// Digit is 5, 6 or NaN
+				switch (thirdLine) {
+				case " _|":
+					digit = '5';
+					break;
+				case "|_|":
+					digit = '6';
+					break;
+				}
+				break;
+			case "  |":
+				// Digit is 7 or NaN
+				if (thirdLine.equals("  |")) {
+					digit = '7';
+				}
+				break;
+			case "| |":
+				// Digit is 0 or NaN
+				if (thirdLine.equals("|_|")) {
+					digit = '0';
+				}
+				break;
+			}
+			break;
+		}
+		return digit;
+	}
+
+	public static String getTotalResult() {
+		return totalResult;
+	}
+
+}
---
+++ SevenSegments/src/test/CoreTest.java
@@ -0,0 +1,161 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+
+import logical.Core;
+
+/**
+ * Test class of Core class
+ */
+class CoreTest {
+
+	/**
+	 * Class to be tested
+	 */
+	private Core core;
+	
+	
+	/**
+	 * Scenario that has all numbers from 0 to 9
+	 */
+	private String scenario1Main = "data/scenario1.txt";
+
+	/**
+	 * Scenario that has segments that are not numbers
+	 */
+	private String scenario2Main = "data/scenario2.txt";
+
+	/**
+	 * Scenario where the lines do not have the same size
+	 */
+	private String scenario3Main = "data/scenario3.txt";
+
+	/**
+	 * Scenario with only one digit
+	 */
+	private String scenario4Main = "data/scenario4.txt";
+
+	/**
+	 * Scenario with more than 3 lines, but multiple of 3 therefore correct
+	 */
+	private String scenario5Main = "data/scenario5.txt";
+
+	/**
+	 * Test that all possible digits are converted to string correctly
+	 */
+	@org.junit.jupiter.api.Test
+	public void testScenario1() {
+		String[] args = null;
+		final InputStream original = System.in;
+		FileInputStream fips;
+		try {
+			fips = new FileInputStream(new File(scenario1Main));
+			System.setIn(fips);
+			core.main(args);
+			String res = Core.getTotalResult();
+			System.setIn(original);
+			assertNotNull(res);
+			assertEquals("1234567890", res);
+			res = "";
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test if a segment is not a number, in this case must show x
+	 */
+	@org.junit.jupiter.api.Test
+	public void testScenario2() {
+		String[] args = null;
+		final InputStream original = System.in;
+		FileInputStream fips;
+		try {
+			fips = new FileInputStream(new File(scenario2Main));
+			System.setIn(fips);
+			core.main(args);
+			String res = Core.getTotalResult();
+			System.setIn(original);
+			assertNotNull(res);
+			assertEquals("12345678xx", res);
+			res = "";
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	* Test if the 3 lines have different size 
+	*/
+	@org.junit.jupiter.api.Test
+	public void testScenario3() {
+		String[] args = null;
+		final InputStream original = System.in;
+		FileInputStream fips;
+		try {
+			fips = new FileInputStream(new File(scenario3Main));
+			System.setIn(fips);
+			core.main(args);
+			String res = Core.getTotalResult();
+			System.setIn(original);
+			assertNotNull(res);
+			assertEquals("For the input you must enter 3 lines of the same size", res);
+			res = "";
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test only one digit
+	 */
+	@org.junit.jupiter.api.Test
+	public void testScenario4() {
+		String[] args = null;
+		final InputStream original = System.in;
+		FileInputStream fips;
+		try {
+			fips = new FileInputStream(new File(scenario4Main));
+			System.setIn(fips);
+			core.main(args);
+			String res = Core.getTotalResult();
+			System.setIn(original);
+			assertNotNull(res);
+			assertEquals("0", res);
+			res = "";
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * Test for cases where there are more than 3 lines
+	 */
+	@org.junit.jupiter.api.Test
+	public void testScenario5() {
+		String[] args = null;
+		final InputStream original = System.in;
+		FileInputStream fips;
+		try {
+			fips = new FileInputStream(new File(scenario5Main));
+			System.setIn(fips);
+			core.main(args);
+			String res = Core.getTotalResult();
+			System.setIn(original);
+			assertNotNull(res);
+			assertEquals("433805825433805825\r\n" + "1234567890\r\n" + "0", res);
+			res = "";
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
